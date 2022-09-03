31f79f61610158e2f528e524e6265e2a3c0920db
senti <brianmv@uci.edu>
2022-09-02 22:46:37 -0700

    forms are so easy

--- web/src/components/Header/NavLinks.svelte
+++ web/src/components/Header/NavLinks.svelte
@@ -6,17 +6,17 @@
   <li class=""><a href="/">Datasets</a></li>
   <li class="dropdown dropdown-hover">
     <a href="/">
-      <span class="hidden lg:block"> Donate a Dataset </span>
-      <span class="lg:hidden"> Contribute a Dataset </span>
+      <a href="/donation" class="hidden lg:block"> Donate a Dataset </a>
+      <a href="/donation" class="lg:hidden"> Contribute a Dataset </a>
       <CaretIcon class="rotate-90 block lg:hidden" />
     </a>
     <ul
       tabindex="0"
       class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 lg:hidden"
     >
-      <li><a href="/">Donate a Dataset</a></li>
-      <li><a href="/">Link an External Dataset</a></li>
+      <li><a href="/donation">Donate a Dataset</a></li>
+      <li><a href="/donation">Link an External Dataset</a></li>
     </ul>
   </li>
-  <li class="hidden lg:block"><a href="/">Link a Dataset</a></li>
+  <li class="hidden lg:block"><a href="/donation">Link a Dataset</a></li>
 </ul>
